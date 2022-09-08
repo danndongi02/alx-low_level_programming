5b114f6663be8dc97a411cfbbd9c3cfb22568588
danndongi02 <ianndunguh@gmail.com>
2022-09-08 23:04:35 +0000

    #003: Print using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,6 @@ int main(void)
 	/*
 	 * This is a program that prints using puts
 	 */
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
