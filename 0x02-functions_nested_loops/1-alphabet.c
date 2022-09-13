5b4ff2310af5480fb5fe39970b5faa094a91121a
danndongi02 <ianndunguh@gmail.com>
2022-09-13 17:04:50 +0000

    #001: Corrected the error on line 13

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,16 @@
 #include "main.h"
 
+/**
+ * print_alphabet - Prints all the lowercase alphabets
+ *
+ */
 void print_alphabet(void)
 {
-    char ch;
+	char ch;
 
-    for (ch = 'a'; ch <= 'z'; ch++)
-    {
-        putchar(ch)
-    }
-    putchar('\n');
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
 }
