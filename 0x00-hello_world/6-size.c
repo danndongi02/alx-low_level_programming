7c1e7029aa3495ce519c386b49aab5e7b0ce5428
danndongi02 <ianndunguh@gmail.com>
2022-09-13 02:59:06 +0000

    Check numbers

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- *  main - Prrint the size of various types on the computer it is compiled on
- *   Return: 0
- */
+* main - Prrint the size of various types on the computer it is compiled on
+*
+* Return: 0
+*/
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
