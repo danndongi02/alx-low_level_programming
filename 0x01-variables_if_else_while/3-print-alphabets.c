a4d890bdad9cacbe1898102bf388b1ccd48da618
danndongi02 <ianndunguh@gmail.com>
2022-09-13 04:20:52 +0000

    Print all alphabets

---
+++ 0x01-variables_if_else_while/3-print-alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print all alphabets
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
