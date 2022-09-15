be7e755d4615b228d942f2b38bceaa72847e736b
danndongi02 <ianndunguh@gmail.com>
2022-09-15 21:42:10 +0000

    Added function description

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * add - return sum of two numbers
+ *
+ * @x: first integer
+ * @y: second integer
+ * Return: sum of x and y
+ */
 int add(int x, int y)
 {
 	return (x + y);
