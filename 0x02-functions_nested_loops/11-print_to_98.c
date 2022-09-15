41cc91cfc84c6833e8fed411d1ecb6f1469ba8d8
danndongi02 <ianndunguh@gmail.com>
2022-09-15 22:15:52 +0000

    Added extra if block

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,13 +8,15 @@
 void print_to_98(int n)
 {
 	int i;
-	
+
 	if (n > 98)
 	{
 
-		for (i = n; i >= 98; i++)
+		for (i = n; i >= 98; i--)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
 		}
 	}
 	else
@@ -22,6 +24,8 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			printf("%d, ", i);
+			if (i != 98)
+				printf(", ");
 		}
 	}
 	printf("\n");
