169bf778f4faf0c7b3410a678c5409708d851406
danndongi02 <ianndunguh@gmail.com>
2022-09-16 10:21:28 +0000

    Print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * @brief print_line - Print line
+ *
+ * @n: number of times '_' should be
+ * printed
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
