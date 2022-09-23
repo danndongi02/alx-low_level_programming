11bf758f807e0789c539fc12a4f19659130bf180
danndongi02 <ianndunguh@gmail.com>
2022-09-23 17:41:38 +0000

    Print all letters apart from 'q' and 'e'

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print all letters apart from q and e
+ *
+ * Return: 0 (Always success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (!(ch == 'q' || ch == 'e'))
+		{
+			putchar(ch);
+		}
+	}
+
+	return (0);
+}
