6283eb7a973691bd55b8c8f233f9a1bcdec0d274
danndongi02 <ianndunguh@gmail.com>
2022-09-25 11:44:38 +0000

    Changed the loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,16 +9,20 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	for (i = 0; i < n; i++)
+	if (n > 0)
 	{
-		if (n < 0)
-			_putchar('\n');
-
-		for (j = 0; j < i; j++)
+		for (i = 0; i < n; i++)
 		{
-			_putchar(' ');
+			if (n < 0)
+				_putchar('\n');
+
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
 		}
-		_putchar('\\');
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
