4e00e88a1383dd679a928bbdc718521a4daacd7a
danndongi02 <ianndunguh@gmail.com>
2022-10-09 20:23:28 +0000

    Print multiples of n

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+void print_times_table(int n)
+{
+	int i, j, result;
+	if (!(n < 0 || n > 15))
+	{
+		for (i = 0; i < n + 1; i++)
+		{
+			for (j = 0; j < n + 1; j++)
+			{
+				result = i * j;
+				if (result > 9 && result < 100)
+				{
+					_putchar(result / 10 + '0');
+					_putchar(result % 10 + '0');
+				}
+				else if (result > 100)
+				{
+					_putchar(result / 100 + '0');
+					_putchar((result / 10) % 10 + '0');
+					_putchar(result % 10 + '0');
+				}
+				else
+				{
+					_putchar(result + '0');
+				}
+
+				if (j != n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
