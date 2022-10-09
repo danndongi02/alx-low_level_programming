86963e23af1a964867b4974b212494fe58aa173e
danndongi02 <ianndunguh@gmail.com>
2022-10-09 20:31:04 +0000

    Changed spacing

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,8 +1,14 @@
 #include "main.h"
 
+/**
+ * print_times_table - Print times table of n
+ *
+ * @n: times table
+ */
 void print_times_table(int n)
 {
 	int i, j, result;
+
 	if (!(n < 0 || n > 15))
 	{
 		for (i = 0; i < n + 1; i++)
@@ -12,6 +18,7 @@ void print_times_table(int n)
 				result = i * j;
 				if (result > 9 && result < 100)
 				{
+					_putchar(' ');
 					_putchar(result / 10 + '0');
 					_putchar(result % 10 + '0');
 				}
@@ -23,13 +30,13 @@ void print_times_table(int n)
 				}
 				else
 				{
+					_putchar(' ');
 					_putchar(result + '0');
 				}
 
 				if (j != n)
 				{
 					_putchar(',');
-					_putchar(' ');
 					_putchar(' ');
 				}
 			}
