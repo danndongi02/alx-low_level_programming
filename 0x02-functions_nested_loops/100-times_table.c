11fad1ffb16323dff3df80f31d2c65fb57ad52d6
danndongi02 <ianndunguh@gmail.com>
2022-10-09 20:34:41 +0000

    Changed the spacing and ranges

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,7 +22,7 @@ void print_times_table(int n)
 					_putchar(result / 10 + '0');
 					_putchar(result % 10 + '0');
 				}
-				else if (result > 100)
+				else if (result > 99)
 				{
 					_putchar(result / 100 + '0');
 					_putchar((result / 10) % 10 + '0');
@@ -30,6 +30,7 @@ void print_times_table(int n)
 				}
 				else
 				{
+					_putchar(' ');
 					_putchar(' ');
 					_putchar(result + '0');
 				}
