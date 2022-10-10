f67bb42b9188174dd7971d5d875af96e885adbad
danndongi02 <ianndunguh@gmail.com>
2022-10-10 09:37:37 +0000

    FizzBuzz challenge

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int count;
+
+	for (count = 1; count < 101; count++)
+	{
+		if ((count % 3 == 0) && (count % 5 == 0))
+			printf("FizzBuzz ");
+		else if (count % 3 == 0)
+			printf("Fizz ");
+		else if (count % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", count);
+	}
+
+	printf("\n");
+
+	return (0);
+}
