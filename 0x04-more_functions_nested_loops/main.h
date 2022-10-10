94ab82fe6311957bd6ab784586d953ef56961df0
danndongi02 <ianndunguh@gmail.com>
2022-10-10 09:23:52 +0000

    Added new function

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,8 +2,9 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include "../0x02-functions_nested_loops/001-putchar.c"
 
-void _putchar(char c);
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -12,5 +13,6 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int);
+void print_square(int size);
 
 #endif /* MAIN_H */
