f083da53f7e0d4ab163d8ded2fca5bffa5ec6410
danndongi02 <ianndunguh@gmail.com>
2022-10-14 19:30:52 +0000

    Changed value of height

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,7 +11,7 @@ void print_triangle(int size)
 
 	if (size > 0)
 	{
-		for (height = 0; height <= size; height++)
+		for (height = 1; height <= size; height++)
 		{
 			for (space = size; space > height; space--)
 			{
