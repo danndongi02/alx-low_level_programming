863432bf34a413f21217df24b881dc9677855477
danndongi02 <ianndunguh@gmail.com>
2022-10-14 22:20:45 +0000

    Print the reverse of a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - print the reverse of a string
+ *
+ * @s: sring to be reversed
+ */
+void print_rev(char *s)
+{
+	int length = 0, index;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	for (index = length; index >= 0; index--)
+	{
+		_putchar(s[index]);
+	}
+}
