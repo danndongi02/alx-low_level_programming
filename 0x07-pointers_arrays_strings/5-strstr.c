47b9c40cabe5f4525bbdd720525b4ca89df46430
danndongi02 <ianndunguh@gmail.com>
2022-11-19 10:49:38 +0000

    Serach for first occurrence of substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strstr - find the first occurrence of substring needle
+ *
+ * @haystack: string to be checked
+ * @needle: substring to search for
+ *
+ * Return: 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int index = 0;
+
+	while (*haystack)
+	{
+		if (haystack[index] == needle[index])
+		{
+			while (haystack[index] == needle[index])
+			{
+				if (needle[index + 1] == '\0')
+					return (haystack);
+
+				index++;
+			}
+		}
+
+		haystack++;
+	}
+
+	return ('\0');
+}
