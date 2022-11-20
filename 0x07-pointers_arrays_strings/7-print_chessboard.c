32d15bf91b8df462a0e66ecec3877d6ec04ee691
danndongi02 <ianndunguh@gmail.com>
2022-11-20 08:44:58 +0000

    Print the chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print the chessboard
+ *
+ * @a: chessboard to be printed
+ */
+void print_chessboard(char (*a)[8])
+{
+	int pos1, pos2;
+
+	for (pos1 = 0; a[pos1][7]; pos1++)
+	{
+		for (pos2 = 0; pos2 < 8; pos2++)
+		{
+			_putchar(a[pos1][pos2]);
+		}
+	}
+}
